42e24a7b43ac16a1de42252a26943d9ccdebdf57
Bo <boprojectbo@gmail.com>
2024-09-03 13:46:08 +0200

    Feat: _components and sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,12 @@
+import Sidebar from "./_components/sidebar";
+
+
 const DashboardLayout = ({children} : {children: React.ReactNode}) => {
     return ( 
-        <div>
+        <div className="h-full">
+            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0">
+                <Sidebar />
+            </div>
             {children}
         </div>
      );
