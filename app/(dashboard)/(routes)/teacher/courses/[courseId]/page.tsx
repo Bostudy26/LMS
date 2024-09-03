23023cfad7f5424e9e83699a8e53258d96194b0a
Bo <boprojectbo@gmail.com>
2024-09-03 23:14:03 +0200

    Feat: CourseIdPage

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,9 @@
+const CourseIdPage = ({params}: {params: {courseId: string}}) => {
+    return ( 
+        <div>
+            course Id {params.courseId}
+        </div>
+     );
+}
+ 
+export default CourseIdPage;
