7c5b37cc1d2cb649fdf3605ad3c3293cb4dd4431
Bo <boprojectbo@gmail.com>
2024-09-05 21:02:53 +0200

    feat: shadcn Alert dialog & chapter actions

--- components/banner.tsx
+++ components/banner.tsx
@@ -35,6 +35,7 @@ export const Banner = ({
     return (
         <div className={cn(bannerVariants({ variant}))}>
             <Icon className="h-4 w-4 mr-2" />
+            {label}
         </div>
     )
 }
