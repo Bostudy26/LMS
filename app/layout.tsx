5220c338ef09fd413eb600edbddf61e452cf90c9
Bo <boprojectbo@gmail.com>
2024-09-06 10:37:35 +0200

    Feat: confetti store and provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Toasterprovider from "@/components/providers/toaster-provider";
+import Confetti from "react-confetti/dist/types/Confetti";
+import { ConfettiProvider } from "@/components/providers/confetti-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +22,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ConfettiProvider />
           <Toasterprovider />
           {children}
         </body>
