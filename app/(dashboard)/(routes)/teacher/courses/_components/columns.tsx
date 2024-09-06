b4d7c51c6aeec8482f52a531aa4092993f852f20
Bo <boprojectbo@gmail.com>
2024-09-06 11:53:28 +0200

    add Course Table

---
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+
+export type Payment = {
+  id: string
+  amount: number
+  status: "pending" | "processing" | "success" | "failed"
+  email: string
+}
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "title",
+    header: ({column}) => {
+        return (
+            <Button
+              variant="ghost"
+              onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+              Title
+              <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+          )
+    },
+  },
+  {
+    accessorKey: "price",
+    header: ({column}) => {
+        return (
+            <Button
+              variant="ghost"
+              onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+              Price
+              <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+          )
+    },
+  },
+  {
+    accessorKey: "isPublished",
+    header: ({column}) => {
+        return (
+            <Button
+              variant="ghost"
+              onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+              Published
+              <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+          )
+    },
+  },
+]
