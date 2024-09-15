ee24da58ab352f32522c64a53120b22a3a4ec643
Bo <boprojectbo@gmail.com>
2024-09-15 11:15:13 +0200

    Feat: Get Analytics + card+ data+card | shadcn card

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,6 +1,38 @@
-const AnalyticsPAge = () => {
+import { getAnalytics } from "@/actions/get-analytics";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { DataCard } from "./_components/data-card";
+
+
+ 
+
+const AnalyticsPAge = async () => {
+    const { userId } = auth();
+
+    if (!userId){
+        return redirect("/");
+    }
+
+    const {
+        data,
+        totalRevenue,
+        totalSales,
+    } = await getAnalytics(userId);
+
     return (
-        <div>Analytics</div>
+        <div className="p-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md-4">
+                <DataCard
+                    value={totalRevenue}
+                    label="Total Revenue"
+                    shouldFormat
+                />  
+                <DataCard
+                    value={totalSales}
+                    label="Total Sales"
+                />    
+            </div>
+        </div>
     );
 }
  
